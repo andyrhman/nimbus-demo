17ff47a0406a260da725679e8a3bffe75b0bf05e
andyrhman <andyrhmann@gmail.com>
2024-12-03 20:50:59 +0800

    Fixing more issue

--- src/pages/destinasi/[id].jsx
+++ src/pages/destinasi/[id].jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { useRouter } from 'next/router';
+
 import Layout from '@/components/Layout';
 import SEO from '@/components/SEO';
 import UserWrapper from '@/components/UserWrapper.js';
 
 const DestinasiWisata = () => {
     const pageTitle = `Destinasi Wisata | ${process.env.siteTitle}`
+    const [destinasiWisata, setDestinasiWista] = useState('');
+    const [categoryWisata, setCategoryWisata] = useState('');
+    const [provinsi, setProvinsi] = useState('');
+
+    const [loading, setLoading] = useState(true);
+
+    const router = useRouter();
+    const { id } = router.query;
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        if (id) {
+            (
+                async () => {
+                    setLoading(true);
+                    try {
+                        const { data } = await axios.get(`user/tempat-wisata/${id}`);
+                        setDestinasiWista(data);
+                        setCategoryWisata(data.categoryWisata);
+                        setProvinsi(data.provinsi);
+                        setLoading(false);
+
+                    } catch (error) {
+                        console.log(error);
+                    } finally {
+                        setLoading(false);
+                    }
+                }
+            )();
+        }
+    }, [id]);
 
     return (
         <Layout>
